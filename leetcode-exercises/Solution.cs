b848e155ebf82e11cf48258683d1757af8e0b3aa
joohxz <joohxz@gmail.com>
2024-08-30 02:39:09 -0300

    Add project files.

---
+++ leetcode-exercises/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class Solution
+    {
+        //Palindrome Number
+        //Gives a int number and verifys if it is a palindrome (returns true else false)
+        public bool IsPalindrome(int x)
+        {
+            string num = x.ToString();
+            string numPalindrome = "";
+            int count = (num.Length - 1);
+            for (int i = 0; i <= count; count--)
+            {
+                numPalindrome += num[count];
+            }
+            if (numPalindrome.Equals(num))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
